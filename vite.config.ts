82a0cee1ad7a12d96d4447ee2ea3e74f6645d478
a920604a <a920604a@gmail.com>
2025-05-21 09:25:15 +0800

    🚀 Frontend GitHub Pages 部署設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// 替換為你的 GitHub Repo 名稱
+const repoName = 'house_frontend'
+
 export default defineConfig({
+  base: `/${repoName}/`, // GitHub Pages 路徑
   plugins: [react()],
 })
